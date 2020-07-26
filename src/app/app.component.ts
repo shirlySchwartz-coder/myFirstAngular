d9b7af2b2d0645eeb7c01d09be39827d5713e92e
shirlySchwartz-coder <s0577611175@gmail.com>
2020-07-26 18:40:07 +0300

    start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'myapp';
+  name :string = 'shirly';
+  age:number = 42;
 }
