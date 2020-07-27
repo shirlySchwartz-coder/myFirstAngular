f91ac9a60ab75eb773cc3701085048efb4325f72
shirlySchwartz-coder <s0577611175@gmail.com>
2020-07-27 19:59:04 +0300

    a

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  name :string = 'shirly';
+ /*name :string = 'shirly';
   age:number = 42;
+  x:string ="helloworld";
+  //color:string="red";
+  classColor:string = "green";*/
+  mcolor:string ="red";
+
+  handelClick(event:MouseEvent):void {
+    
+  }
 }
